1c963fcc9bbd72dd1b54490abcb8a3c751b4af21
JoshVC30 <joshuavc30@gmail.com>
2025-01-24 05:53:49 -0500

    Main and Player

--- bobaeria-cafe/Player/static_body_3d.gd
+++ bobaeria-cafe/Player/static_body_3d.gd
@@ -1,21 +1,15 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
 
+var pickedObject
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -24,7 +18,19 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-	
-
-
+		
 	move_and_slide()
+
+func _input(event):
+	if event.is_action_pressed("quit"):get_tree().quit()
+	
+	if event.is_action_pressed("interaction") and pickedObject:
+		pickedObject.reparent(get_tree().current_scene)
+		pickedObject = null
+
+func pick_up_object(object):
+	object.reparent(self)
+	object.global_position = %CarryTeaMarker.global_position
+	
+	await get_tree().create_timer(0.1).timeout
+	pickedObject = object
