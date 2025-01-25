97467aa60b4d67b17147cc7e6575c328e44c64a7
PantsuSSB <149723477+PantsuSSB@users.noreply.github.com>
2025-01-24 22:52:51 -0500

    push

--- bobaeria-cafe/UI/DrinkConstructorMenu/Scripts/radio_buttons_controller.gd
+++ bobaeria-cafe/UI/DrinkConstructorMenu/Scripts/radio_buttons_controller.gd
@@ -1,5 +1,9 @@
 extends Node
 
+#func _ready() -> void:
+	#for child in self.get_children():
+		#if (child is CheckBox):
+			#child.to
 #toggles button on and off based on connected checkbox.toggled signals
 #added button name to connected checkbox.toggled signals
 #the signal needs to be updated if the button's name is changed
