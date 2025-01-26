1a5c5475fe12c4225fae652f6a3cd9851f28b760
Kamjam <15620342+Kamjam@users.noreply.github.com>
2025-01-25 21:46:26 -0500

    Splash Screen and new imports :)
    -added splash screen, which is now default scene
    -imported some sfx
    -imported fonts
    -imported some shaders
    -imported panel styles

---
+++ bobaeria-cafe/Menus/splashscreen.gd
@@ -0,0 +1,12 @@
+#https://youtu.be/E4ybJVTXq1A
+extends Control
+
+@export var changescenetimer:Timer
+@export var audioplayer:AudioStreamPlayer2D
+
+func _ready():
+	changescenetimer.start()
+
+func _on_timer_timeout() -> void:
+	print('change scene')
+	get_tree().change_scene_to_file("res://Scenes/Main.tscn")
