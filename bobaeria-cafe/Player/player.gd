48e398d05a5964e8b4b9de4e2d9eee3525fb7755
flyingcratesdev <joeflydev@gmail.com>
2025-01-26 05:11:08 -0800

    Fix

--- bobaeria-cafe/Player/player.gd
+++ bobaeria-cafe/Player/player.gd
@@ -41,7 +41,7 @@ func _physics_process(delta: float) -> void:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			velocity.z = move_toward(velocity.z, 0, SPEED)
 		
-			move_and_slide()
+		move_and_slide()
 	
 	_player_animation()
 
