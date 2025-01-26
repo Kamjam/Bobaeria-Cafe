a8d4dc07842b0993f81c80dcd44a82d2671f0222
Kamjam <15620342+Kamjam@users.noreply.github.com>
2025-01-26 00:33:54 -0500

    End of Day Screen and Tutorial Overlay 😢

---
+++ bobaeria-cafe/Menus/tutorial-controls.gd
@@ -0,0 +1,44 @@
+extends CanvasLayer
+
+@export var animator:AnimationPlayer
+
+var is_open:bool = false
+var first_steps:bool = false
+
+func _ready():
+	self.visible = false
+	pass
+
+func _process(delta):
+	check_first()
+	if Input.is_action_just_pressed("toggle_tutorial_overlay"):
+		if(is_open):
+			close_tutorial()
+		else:
+			open_tutorial()
+
+func check_first():
+		if not first_steps and \
+		(Input.is_action_pressed("up") or \
+		Input.is_action_pressed("left") or \
+		Input.is_action_pressed("down") or \
+		Input.is_action_pressed("right") or \
+		Input.is_mouse_button_pressed(1) or \
+		Input.is_mouse_button_pressed(2)):
+			open_tutorial()
+			first_steps = true
+
+func close_tutorial():
+		animator.play("panel_fade_out")
+		await animator.animation_finished
+		get_tree().paused = false
+		self.visible = false
+		is_open = false
+		print("Is the tut hidden? " + str(!is_open))
+
+func open_tutorial():
+		get_tree().paused = true
+		self.visible = true
+		animator.play("panel_fade_in")
+		is_open = true
+		print("Is the tut hidden? " + str(!is_open))
